a740a95ac20ac17ac5f902c2ceae122ef104b31d
Maksim <alesinmaks007@gmail.com>
2025-12-02 18:02:35 +0300

    задание 3

--- kt14/Program.cs
+++ kt14/Program.cs
@@ -1,86 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace kt12
+namespace kt14
 {
-    class SimpleCalculator
+    class SimpleCountryInfo
     {
-        static Dictionary<char, int> priorities = new Dictionary<char, int>
-    {
-        {'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}
-    };
-
         static void Main()
         {
-            Console.Write("answer ");
-            string input = Console.ReadLine();
+            Dictionary<string, string> capitals = new Dictionary<string, string>();
+            capitals.Add("Россия", "Москва");
+            capitals.Add("США", "Вашингтон");
+            capitals.Add("Германия", "Берлин");
 
-            var postfix = ConvertToPostfix(input);
-            double result = CalculatePostfix(postfix);
+            Dictionary<string, int> populations = new Dictionary<string, int>();
+            populations.Add("Россия", 146);
+            populations.Add("США", 331);
+            populations.Add("Германия", 83);
 
-            Console.WriteLine($"resuеlt {result}");
-        }
+            Console.Write("write name of country ");
+            string country = Console.ReadLine();
 
-        static List<string> ConvertToPostfix(string expr)
-        {
-            var output = new List<string>();
-            var operators = new Stack<char>();
-            string number = "";
-
-            foreach (char c in expr)
+            if (capitals.ContainsKey(country) && populations.ContainsKey(country))
             {
-                if (char.IsDigit(c))
-                {
-                    number += c;
-                }
-                else if (priorities.ContainsKey(c))
-                {
-                    if (number != "")
-                    {
-                        output.Add(number);
-                        number = "";
-                    }
-
-                    while (operators.Count > 0 && priorities[operators.Peek()] >= priorities[c])
-                    {
-                        output.Add(operators.Pop().ToString());
-                    }
-                    operators.Push(c);
-                }
+                Console.WriteLine($"capital: {capitals[country]}");
+                Console.WriteLine($"population: {populations[country]} millions");
             }
-
-            if (number != "") output.Add(number);
-            while (operators.Count > 0) output.Add(operators.Pop().ToString());
-
-            return output;
-        }
-
-        static double CalculatePostfix(List<string> postfix)
-        {
-            var stack = new Stack<double>();
-
-            foreach (string token in postfix)
+            else
             {
-                if (double.TryParse(token, out double num))
-                {
-                    stack.Push(num);
-                }
-                else
-                {
-                    double b = stack.Pop();
-                    double a = stack.Pop();
-
-                    switch (token)
-                    {
-                        case "+": stack.Push(a + b); break;
-                        case "-": stack.Push(a - b); break;
-                        case "*": stack.Push(a * b); break;
-                        case "/": stack.Push(a / b); break;
-                    }
-                }
+                Console.WriteLine("country doesnt exists");
             }
-
-            return stack.Pop();
         }
     }
 }
